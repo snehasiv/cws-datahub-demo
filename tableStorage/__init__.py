039d699182e6cc4b1a77bbde6a4557eab88010d1
Sneha Srivastava <snsrivas@adobe.com>
2022-11-17 13:51:57 +0530

    connection string method update

--- tableStorage/__init__.py
+++ tableStorage/__init__.py
@@ -2,9 +2,8 @@ import os
 from azure.data.tables import TableServiceClient
 from azure.data.tables import TableClient
 
-account_key = os.environ['STORAGE_ACCOUNT_KEY']
-account_name = os.environ['STORAGE_ACCOUNT_NAME']
-connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name}AccountKey={account_key};EndpointSuffix=core.windows.net;BlobEndpoint=https://{account_name}.blob.core.windows.net/;FileEndpoint=https://{account_name}.file.core.windows.net/;QueueEndpoint=https://{account_name}.queue.core.windows.net/;TableEndpoint=https://{account_name}.table.core.windows.net/"
+
+connection_string = os.environ['STORAGE_ACCOUNT_CONNECTION_STRING']
 
 TABLE_SERVICE_CLIENT = TableServiceClient.from_connection_string(conn_str=connection_string)
 
